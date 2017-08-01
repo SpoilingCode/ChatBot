528406207888abfad8ba72e49189231cc79a1e66
Ivanisenko Roman <r.ivanisenko@gmail.com>
2017-08-01 05:56:41 +0300

    added english quotation in storage

--- src/answersstorage/englishanswers/StorageEnglishGeneralPhrases.java
+++ src/answersstorage/englishanswers/StorageEnglishGeneralPhrases.java
@@ -1,7 +1,28 @@
 package answersstorage.englishanswers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * English quotation store
  */
 public class StorageEnglishGeneralPhrases {
+    private List<String> englishGeneralPhrases;
+
+    public StorageEnglishGeneralPhrases() {
+        englishGeneralPhrases = new ArrayList<String>(){{
+            add("An honest answer is the sign of true friendship.");
+            add("Two things are infinite: the universe and human stupidity; and I’m not sure about the universe.");
+            add("One who looks for a friend without faults will have none.");
+            add("Keep cool; anger is not an argument.");
+            add("It is always a silly thing to give advice, but to give good advice is fatal. ");
+            add("Fortune is easily found, but hard to be kept.");
+            add("Fortune is easily found, but hard to be kept.");
+            add("I learned that it is the weak who are cruel, and that gentleness is to be expected only from the strong");
+        }};
+    }
+
+    public List<String> getEnglishGeneralPhrases() {
+        return englishGeneralPhrases;
+    }
 }
